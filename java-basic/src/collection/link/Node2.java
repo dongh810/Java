290bc60b324bfa6d4b055dad015bb0faee4d0b83
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-11-19 00:41:15 +0900

    Node 개념 학습

---
+++ java-basic/src/collection/link/Node2.java
@@ -0,0 +1,32 @@
+package collection.link;
+
+public class Node2 {
+
+    Object item;
+    Node2 next;
+    public Node2(Object item) {
+        this.item = item;
+    }
+    //IDE 생성 toString() @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node2 x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next; }
+        sb.append("]");
+        return sb.toString();
+    }
+}
