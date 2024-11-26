99911b60d5910ddfabf11b24643a485d8b1048ec
dongh810 <baek0810@gachon.ac.kr>
2024-11-26 17:56:45 +0900

    iterator 개념 학습

--- java-basic/src/collection/iterable/MyArrayMain.java
+++ java-basic/src/collection/iterable/MyArrayMain.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 public class MyArrayMain {
 
     public static void main(String[] args) {
-
         MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
         Iterator<Integer> iterator = myArray.iterator();
         System.out.println("iterator 사용");
