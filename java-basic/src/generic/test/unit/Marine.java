8ecb6d2707be796b32387c74ae132c1c0d09d63e
dongh810 <105986200+dongh810@users.noreply.github.com>
2025-02-09 23:35:29 +0900

    제네릭 개념 공부

---
+++ java-basic/src/generic/test/unit/Marine.java
@@ -0,0 +1,10 @@
+package generic.test.unit;
+
+import generic.test.unit.BioUnit;
+
+public class Marine extends BioUnit {
+
+    public Marine(String name, int hp) {
+        super(name, hp);
+    }
+}
